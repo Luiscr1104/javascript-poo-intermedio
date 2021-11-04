dfe67a56284d41e09f76c1fd944a035fd8d49ee5
Luis R Zunniga S <luiscr1104@gmail.com>
2021-11-04 12:49:26 -0600

    metodos static

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function isObject(subject) {
   return typeof subject == "object";
 }
-  
+
 function isArray(subject) {
   return Array.isArray(subject);
 }
@@ -37,63 +37,102 @@ function deepCopy(subject) {
   return copySubject;
 }
 
-/*const studentBase = {
-  name: undefined,
-  email: undefined,
-  age: undefined,
-  approvedCourses: undefined,
-  learningPaths: undefined,
-  socialMedia: {
-    twitter: undefined,
-    facebook: undefined,
-    instagram: undefined,
-  },
-};*/
+function SuperObject() {}
+SuperObject.isObject = function (subject) {
+  return typeof subject == "object";
+}
+SuperObject.deepCopy = function (subject) {
+  let copySubject;
 
-function requiredParam(param) {
-  throw new Error(param+ " Es obligatorio");
+  const subjectIsObject = isObject(subject);
+  const subjectIsArray = isArray(subject);
+
+  if (subjectIsArray) {
+    copySubject = [];
+  } else if (subjectIsObject) {
+    copySubject = {};
+  } else {
+    return subject;
+  }
+
+  for (key in subject) {
+    const keyIsObject = isObject(subject[key]);
+
+    if (keyIsObject) {
+      copySubject[key] = deepCopy(subject[key]);
+    } else {
+      if (subjectIsArray) {
+        copySubject.push(subject[key]);
+      } else {
+        copySubject[key] = subject[key];
+      }
+    }
+  }
+
+  return copySubject;
 }
 
-function createStudent({
+function requiredParam(param) {
+  throw new Error(param + " es obligatorio");
+}
+
+function LearningPath({
   name = requiredParam("name"),
-  age,
+  courses = [],
+}) {
+  this.name = name;
+  this.courses = courses;
+}
+
+function Student({
+  name = requiredParam("name"),
   email = requiredParam("email"),
+  age,
   twitter,
   instagram,
   facebook,
   approvedCourses = [],
   learningPaths = [],
-}={}) {
+} = {}) {
+  this.name = name;
+  this.email = email;
+  this.age = age;
+  this.approvedCourses = approvedCourses;
+  this.socialMedia = {
+    twitter,
+    instagram,
+    facebook,
+  };
+
   const private = {
-    "_name": name,
-  };
-  
-  const public = {
-    email,
-    age,
-    approvedCourses,
-    learningPaths,
-    socialMedia: {
-      twitter,
-      instagram,
-      facebook,
-    },
-    changeName(newName){
-      private["_name"] = newName;
-    },
-    readName(){
-      return private["_name"];
-    },
+    "_learningPaths": [],
   };
 
-Object.defineProperty(public, "readName", {
-  configurable: false,
-  writable: false,
-});
+  Object.defineProperty(this, "learningPaths", {
+    get() {
+      return private["_learningPaths"];
+    },
+    set(newLp) {
+      if (newLp instanceof LearningPath) {
+        private["_learningPaths"].push(newLp);
+      } else {
+        console.warn("Alguno de los LPs no es una instancia dell prototipo LearningPath");
+      }
+    },
+  });
 
-  return public;
+  for (learningPathIndex in learningPaths) {
+    this.learningPaths = learningPaths[learningPathIndex];
+  }
 }
 
-const juan = createStudent({name: "Juanito", email: "repo@gas.com"});
-
-
+const escuelaWeb = new LearningPath({ name: "Escuela de WebDev" });
+const escuelaData = new LearningPath({ name: "Escuela de Data Science" });
+const juan = new Student({
+  email: "juanito@frijoles.co",
+  name: "Juanito",
+  learningPaths: [
+    escuelaWeb,
+    escuelaData,
+  ],
+});
